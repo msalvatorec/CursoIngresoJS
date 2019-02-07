895bae7c6616f523645ae5bc58fa69551ff6107e
alumno <alumno>
2019-02-06 21:54:08 -0300

    Ejercicio 1 PARCIAL hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+	var ancho = prompt("Ingrese el ancho del rectángulo");
+	var largo = prompt("Ingrese el largo del rectángulo");
 
+	var perimetro;
+
+	perimetro = ancho*2 + largo*2;
+
+	alert("El perimetro es de: "+perimetro);
+	
 }
