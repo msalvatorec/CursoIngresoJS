940e982776a9626212aa437b1251031d5756310d
alumno <alumno>
2019-02-11 20:01:36 -0300

    Ejercicio 3 del IF - HECHOD

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
+
+
+
+if (laEdad>17)
+	{
+		alert("Es mayor de edad");
+	}
+
+if (laEdad<18)
+	{
+		alert("No es mayor de edad");
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
