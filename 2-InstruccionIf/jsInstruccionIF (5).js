e090d17ae84b4d18913bba0adcff1716d9c0b56e
alumno <alumno>
2019-02-11 20:23:22 -0300

    Ejercicio 5 del IF (ejemplo or con pipes)

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
 
+if (laEdad>17 || laEdad<13)
+	{
+		alert("La persona NO es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
