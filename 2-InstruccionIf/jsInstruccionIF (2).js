cf84b8f6550538260220a2e9f48c0239b47fb28d
alumno <alumno>
2019-02-11 19:49:40 -0300

    Ejercicio 2 del IF - HECHOD

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
+
+
+if (laEdad>17)
+	{
+		alert("Es mayor de edad");
+	}
+else
+	{
+		alert("No es mayor de edad");
+	}
+
 
 
 
