be3cf7df45d070050ccbf0bc36a16d0e0f2a61e7
alumno <alumno>
2019-02-11 20:19:56 -0300

    Ejercicio 4 del IF - HECHO Mejorado con (&&)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -6,30 +6,10 @@ laEdad	= parseInt(document.getElementById("edad").value);
 
 
 
-if (laEdad==17)
+if (laEdad<18 && laEdad>12)
 	{
 		alert("La persona es adolescente");
 	}
 
-
-if (laEdad==16)
-	{
-		alert("La persona es adolescente");
-	}
-
-if (laEdad==15)
-	{
-		alert("La persona es adolescente");
-	}
-
-if (laEdad==14)
-	{
-		alert("La persona es adolescente");
-	}
-
-if (laEdad==13)
-	{
-		alert("La persona es adolescente");
-	}
 
 }//FIN DE LA FUNCIÓN
