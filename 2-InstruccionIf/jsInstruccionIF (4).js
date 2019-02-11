f189a9c26b25748252dd8413aa1d0abc3a1d305c
alumno <alumno>
2019-02-11 20:07:22 -0300

    Ejercicio 4 del IF - HECHO (Solo con IF's)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,35 @@
 function mostrar()
 {
 //tomo la edad  
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
 
 
+
+if (laEdad==17)
+	{
+		alert("La persona es adolescente");
+	}
+
+
+if (laEdad==16)
+	{
+		alert("La persona es adolescente");
+	}
+
+if (laEdad==15)
+	{
+		alert("La persona es adolescente");
+	}
+
+if (laEdad==14)
+	{
+		alert("La persona es adolescente");
+	}
+
+if (laEdad==13)
+	{
+		alert("La persona es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
