03da7cce498955c5c0d879c4e69c10d8ba0afdde
alumno <alumno>
2019-02-11 21:21:00 -0300

    Ejercicio 6, solo con IF sin anhidar

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
-//tomo la edad  
 
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
+
+if (laEdad>17)
+{
+	alert("Es mayor de edad");
+}
+
+if (laEdad>12 && laEdad	<18)
+{
+	alert("Es un adolescente");
+}
+
+if (laEdad<13)
+{
+	alert("Es un niño");
+}
 
 
 
