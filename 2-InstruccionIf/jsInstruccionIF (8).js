fb9bfccf0ec78a5f1d95ecff2dd9023e84f14de9
alumno <alumno>
2019-02-11 21:39:05 -0300

    Ejercicio 8 - HECHO

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
+
+	   	
+var elEstadoCivil;
+elEstadoCivil= document.getElementById("estadoCivil").value;
+
+if (laEdad>17 && elEstadoCivil=="Soltero")
+{
+	alert("Es soltero y no es meenor");
+}
 	
 
 
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+var numeroAleatorio = Math.random();
+
+alert(numeroAleatorio);
 
 }//FIN DE LA FUNCIÓN
