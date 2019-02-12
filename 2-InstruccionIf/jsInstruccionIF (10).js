9a134c95d71dcc03b1bbd6221f576780240acb84
alumno <alumno>
2019-02-11 22:15:56 -0300

    Ejercicio 9 - Mejorado, numeros enteros y del 1 al 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+var numeroAleatorio = Math.floor((Math.random() * 10) + 1);;
+
+alert(numeroAleatorio);
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,7 +1,7 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-var numeroAleatorio = 10*Math.random();
+var numeroAleatorio = Math.floor((Math.random() * 10) + 1);
 
 alert(numeroAleatorio);
 
