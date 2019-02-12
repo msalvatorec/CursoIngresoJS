19aa4361ada6e7b01caaa813dda9bc0b0a901165
alumno <alumno>
2019-02-11 21:34:16 -0300

    Ejercicio 7 - HECHO

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -3,6 +3,16 @@ function mostrar()
 //tomo la edad  
 
 	
+var laEdad;
+laEdad	= parseInt(document.getElementById("edad").value);
 
+	   	
+var elEstadoCivil;
+elEstadoCivil= document.getElementById("estadoCivil").value; 
+
+if(laEdad<18 && elEstadoCivil!="Soltero")
+{
+	alert("Es muy pequeño para no ser soltero");
+}
 
 }//FIN DE LA FUNCIÓN
