23f418740c1ebcaf69d710f59ac60e80ff17015d
alumno <alumno>
2019-02-13 19:38:30 -0300

    Primer ejercicio

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,13 +1,22 @@
 
 function mostrar()
 {
-	var ancho = prompt("Ingrese el ancho del rectángulo");
+	/*var ancho = prompt("Ingrese el ancho del rectángulo");
 	var largo = prompt("Ingrese el largo del rectángulo");
 
 	var perimetro;
 
 	perimetro = ancho*2 + largo*2;
 
-	alert("El perimetro es de: "+perimetro);
+	alert("El perimetro es de: "+perimetro);*/
+
+	var primerNombre = prompt("Ingrese primer nombre");
+	var primeraEdad = prompt("Ingrese primer edad");
+	var segundoNombre = prompt("Ingrese segundo nombre");
+	var segundaEdad = prompt("Ingrese segunda edad");
+
+	var laSuma = parseInt(primeraEdad)+parseInt(segundaEdad);
+
+	alert("ustedes son "+primerNombre+" y "+segundoNombre+" sus edades son "+primeraEdad+" y "+segundaEdad+" y la suma de sus edades es "+laSuma);
 	
 }
