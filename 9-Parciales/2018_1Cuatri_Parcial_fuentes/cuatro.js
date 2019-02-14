c20df147dd21ca7efcb8089c805c6762005da495
alumno <alumno>
2019-02-13 21:19:35 -0300

    Ejercicios tipo parcial + Ejercicio 10 IF

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+	/*al realizar una compra, si compra de mas de dos productos 
+	se realiza un descuento del 10% y si supera los $2000 se agrega 
+	un descuento adicional de 15%, si el pago es con tarjeta y no efectivo 
+	al precio final se le agrega un 10% de recargo*/
 
+	var cantidadProductos = prompt("Ingrese cantidad de productos comprados");
+	var importeTotal = prompt("Ingrese importe TOTAL de la compra");
+	var formaDePago = prompt("Ingrese el metodo de pago (en minusculas: tarjeta u cualquier otro)");
+	var importeConSinDescuentos=importeTotal;
+
+	if (cantidadProductos>2)
+	{
+		importeConSinDescuentos=parseInt(importeTotal)*0.9;
+	
+		if (importeTotal>2000)
+		{
+			importeConSinDescuentos=importeConSinDescuentos*.85;
+		}
+
+	}
+
+	if (formaDePago=="tarjeta")
+	{
+		importeConSinDescuentos=importeConSinDescuentos*1.1;
+	}
+
+	alert("El importe total es de: "+importeConSinDescuentos);
+	
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,6 +1,19 @@
 function mostrar()
-{	
+{	/*
 	elNombre
 	laLocalidad
-	alert("usted es "+xxxxx+" y vive en la localidad de "+xxxxxxxx");
+	alert("usted es "+xxxxx+" y vive en la localidad de "+xxxxxxxx");*/
+
+	/*alert("el precio del producto en efectivo es :$ xxxx , 
+	con tarjeta tiene un recargo del 10% que seria $xx de recargo, 
+	costandole final $xxx ")*/
+	var precioProducto	= document.getElementById("elNombre").value;
+	/*var tipoPago = document.getElementById("laLocalidad").value;*/
+
+	var precioAumento = parseInt(precioProducto)*0.1;
+	var precioConAumento = parseInt(precioProducto)*1.1;
+
+
+	alert("el precio del producto en efectivo es: $"+precioProducto+"con tarjeta tiene un recargo del 10% que seria $"+precioAumento+"de recargo costandole final "+precioConAumento);
+
 }
