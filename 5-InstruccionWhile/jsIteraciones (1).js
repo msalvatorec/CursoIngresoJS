da12c6fa51cf0bc5f07bfc6c89655c36de5b83a6
alumno <alumno>
2019-02-25 19:10:16 -0300

    Ejercicio 1 del WHILE

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador=1;
+	alert(contador);
+	while (contador<10) {
 
+  			contador=contador+1;
+  			alert(contador);
+	}
 
 }//FIN DE LA FUNCIÓN
