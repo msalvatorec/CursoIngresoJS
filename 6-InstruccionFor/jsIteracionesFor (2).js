081b30e09e8ab774363e267ecb142e7d4c8779e1
alumno <alumno>
2019-02-25 20:11:35 -0300

    Codigos en el ejercicio 2; saber si un numero ingresado es primo y si un numero ingresado es perfecto (la suma de sus divisores Z>0, inclusive el 1, es igual al numero en cuestion)

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,92 @@
+
 function mostrar()
 {
+	var numeroIngresado = prompt("Por favor ingrese un número");
+	numeroIngresado = parseInt(numeroIngresado);
+	var sumando=0;
+
+
+	for (var numeroAnterior=Math.floor(numeroIngresado/2); numeroAnterior>1; numeroAnterior--)
+	{
+		
+		if ((numeroIngresado%numeroAnterior)==0){
+			sumando=sumando+numeroAnterior;
+		}
+
+	}
+
+	if (sumando+1==numeroIngresado)
+	{
+		alert("El número "+numeroIngresado+" es un número perfecto");
+	}
+	else
+	{
+		alert("El número "+numeroIngresado+" NO es un número perfecto");	
+	}
+}
+
+
+
+/* Ejercicio para buscar todos los numeros primos de un numero ingresado por prompt
+function mostrar()
+{
+	var numeroIngresado = prompt("Por favor ingrese un número");
+	numeroIngresado = parseInt(numeroIngresado);
+	var esPrimo=1;
+
+
+	for (var numeroAnterior=Math.floor(numeroIngresado/2); numeroAnterior>1; numeroAnterior--)
+	{
+		
+		if ((numeroIngresado%numeroAnterior)==0){
+			esPrimo=0;
+			alert("Es divisible por: "+numeroAnterior);
+		}
+
+	}
+
+	if (esPrimo==1)
+	{
+		alert("El número "+numeroIngresado+" es primo");
+	}
 
 
 }
+*/
+
+//Ver de nuevo, no anda nada; es horrible este codigo
+//Nunca entendi que quiso hacer aca...
+
+/*function mostrar()
+{
+	
+	var numeroIngresado = prompt("Por favor ingrese su dato");
+	numeroIngresado = parseInt(numeroIngresado);
+
+	alert("Numero ingresado: "numerosAnteriores);
+
+	for (numerosAnteriores=numeroIngresadogresado-1; numerosAnteriores>1;numerosAnteriores--)
+	{		
+		console.log(numerosAnteriores);	
+		alert(numerosAnteriores);
+
+		if (numeroIngresado%numerosAnteriores)
+		{
+			console.log("Es primo");	
+		}
+	
+
+	}
+
+	if (numerosAnteriores==1)
+	{			
+		console.log("Es primo");
+	}	
+		
+	else
+	{
+		console.log("No es primo");
+	}
+
+
+}*/
